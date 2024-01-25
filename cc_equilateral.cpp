7a06705b0ffc287dbaa8ddb5ff78298e66617326
Harbansh <142929609+hardik44ss@users.noreply.github.com>
2024-01-25 13:25:24 +0530

    Create cc_equilateral.cpp

---
+++ cc_equilateral.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main () 
+{
+     int r;
+    cout<<"Enter number of row : ";
+    cin>>r;
+    
+    for(int i=r;i>0;i--){
+        for (int j = 0; j<i-1; j++)
+        {
+            cout<<" ";
+        }
+        for(int j=r;j>i-1;j--){
+            cout<<"* ";
+        }
+        cout<<"\n";
+        
+    }
+    return 0;
+}
